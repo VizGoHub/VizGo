b787e3807eb513cd557b8f3f3cd26ac497b16a30
mark <ischeck.long@gmail.com>
2025-03-06 12:00:10 +0800

    free

--- executor/SQLExecutor/exec.go
+++ executor/SQLExecutor/exec.go
@@ -6,6 +6,7 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"time" // 导入 time 包以便设置超时
 )
 
 var logger = utils.GetLogger()
@@ -33,15 +34,26 @@ func (ec *SQLExecutor) InitDB() error {
 		}
 	}
 
+	// 获取底层 *sql.DB 对象
+	sqlDB, err := _DB.DB()
+	if err != nil {
+		return err
+	}
+	// 设置连接的最大存活时间，超过该时间连接将被自动关闭（此处设置为10分钟）
+	sqlDB.SetConnMaxLifetime(10 * time.Second)
+	// 可选设置：最大空闲连接数与最大打开连接数
+	sqlDB.SetMaxIdleConns(5)
+	sqlDB.SetMaxOpenConns(10)
+
 	ec.DB = _DB
-	return err
+	return nil
 }
 
 func (ec *SQLExecutor) Query(querySql string) ([]map[string]interface{}, error) {
 	var err error
 	var results []map[string]interface{}
 
-	//logger.Info(querySql)
+	// 执行原生 SQL 查询
 	tx := ec.DB.Raw(querySql).Scan(&results)
 	if tx != nil {
 		return results, err
@@ -50,5 +62,12 @@ func (ec *SQLExecutor) Query(querySql string) ([]map[string]interface{}, error) 
 }
 
 func (ec *SQLExecutor) Free() {
-	ec.DB = nil
+	if ec.DB != nil {
+		// 手动关闭数据库连接
+		sqlDB, err := ec.DB.DB()
+		if err == nil {
+			sqlDB.Close()
+		}
+		ec.DB = nil
+	}
 }
